adf4f7ef9c8b4eb875692771618d1a19411be093
kseniya <kaprisa57@gmail.com>
2022-01-29 09:28:19 +0300

    fix themes export

--- src/config/themes/index.js
+++ src/config/themes/index.js
@@ -1,12 +1,10 @@
-/* eslint-disable no-unused-vars */
-import { default as iduiTheme } from './idui';
-import { default as coffeeTheme } from './coffee';
-import { default as tendernessTheme } from './tenderness';
-import { default as sunsetTheme } from './sunset';
-import { default as seaSandTheme } from './seaSand';
-import { default as eliteTheme } from './elite';
-import { default as autumnTheme } from './autumn';
-import { default as strictBlueTheme } from './strictBlue';
-import { default as berryExplosionTheme } from './berryExplosion';
-import { default as unicornTheme } from './unicorn';
-/* eslint-enable no-unused-vars */
+export { default as iduiTheme } from './idui';
+export { default as coffeeTheme } from './coffee';
+export { default as tendernessTheme } from './tenderness';
+export { default as sunsetTheme } from './sunset';
+export { default as seaSandTheme } from './seaSand';
+export { default as eliteTheme } from './elite';
+export { default as autumnTheme } from './autumn';
+export { default as strictBlueTheme } from './strictBlue';
+export { default as berryExplosionTheme } from './berryExplosion';
+export { default as unicornTheme } from './unicorn';
